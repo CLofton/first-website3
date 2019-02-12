9bf54dcb878bfe3380dad8dae240e720553839bc
Chris Lofton <clofton@austincodingacademy.com>
2019-02-12 13:12:52 -0600

    added JS functionality:

--- script.js
+++ script.js
@@ -6,3 +6,35 @@ function myFunction() {
       x.style.display = "none";
     }
   }
+
+
+  function myFunction() {
+    document.getElementById("hideDive1").innerHTML = "Hello World";
+  }
+
+  function hide1(){
+    var x = document.getElementById("hide1");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+
+  function hide2(){
+    var x = document.getElementById("hide2");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+
+  function hide3(){
+    var x = document.getElementById("hide3");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
